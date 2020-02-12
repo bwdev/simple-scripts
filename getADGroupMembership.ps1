5a4fc80b4910004a10328f58fe1a97bc87c4b6c3
Brian Meek User <bmeek@semgroup.com>
2020-02-12 09:40:53 -0600

    add script for getting AD group membership

---
+++ getADGroupMembership.ps1
@@ -0,0 +1,17 @@
+﻿param([string]$outFileName, [string[]]$groups)
+Import-Module ActiveDirectory
+$fileName = "./$(Get-Date -Format 'yyyyMMdd')_" + $outFileName + ".txt"
+
+$output = ""
+
+foreach ($group in $groups) {    
+    $table = (Get-ADGroupMember -identity $group -Recursive | Get-ADUser -Property DisplayName | Sort-Object DisplayName)
+
+    Write-Host $group
+    $table | Format-List -Property Name, ObjectClass
+
+    if ($outFileName) {
+        $group >> $fileName
+        Get-ADGroupMember -identity $group -Recursive | Get-ADUser -Property DisplayName | Sort-Object DisplayName | Select Name, ObjectClass >> $fileName
+    }
+}
